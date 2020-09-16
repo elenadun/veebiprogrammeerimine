5c30a7de005f5bd2eb71ac50c73d5c2fec2c2225
Elena <55079917+elenadun@users.noreply.github.com>
2020-09-16 21:08:36 +0300

    Update home.php

--- home.php
+++ home.php
@@ -1,26 +1,75 @@
 <?php
+  //loeme andmebaasi login ifo muutujad
+  require("../../config.php");
+  //kui kasutaja on vormis andmeid saatnud, siis salvestame andmebaasi
+  $database = "if20_elena_d_1";
+  if(isset($_POST["submitnonsens"])){
+	  if(!empty($_POST["nonsens"])){
+		  //andmebaasi lisamine
+		  //loome andmebaasi ühenduse
+		  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+		  //valmistame ette SQL käsu
+		  $stmt = $conn->prepare("INSERT INTO nonsens (nonsensidea) VALUES(?)");
+		  echo $conn->error;
+		  //s - string, i -integer, d-decimal
+		  $stmt->bind_param("s", $_POST["nonsens"]);
+		  $stmt->execute();
+		  //käsk ja ühendus sulgeda
+		  $stmt->close();
+		  $conn->close();
+	  } 
+  }
+  
+  //loeme andmebaasist
+  $nonsenshtml = "";
+  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+  //valmistame ette SQL käsu
+  $stmt = $conn->prepare("SELECT nonsensidea FROM nonsens");
+  echo $conn->error;
+  //seome tulemuse mingi muutujaga
+  $stmt->bind_result($nonsensfromdb);
+  $stmt->execute();
+  //võtan, kuni on
+  while($stmt->fetch()){
+	  //<p>suvaline mõte </p>
+	  $nonsenshtml .= "<p>" .$nonsensfromdb ."</p>";
+  }
+  $stmt->close();
+  $conn->close();
+  //ongi andmebaasisit loetud
+
   $username = "Elena Dunajeva";
   $fulltimenow = date("d.m.Y H:i:s");
   $hournow = date("H");
   $partofday = "lihtsalt aeg";
   
-  if($hournow <= 6){
-	  $partofday = "uneaeg";
-  }//enne 6
-  if($hournow >= 8 and $hournow <= 16){
-      $partofday = "õppimise aeg";
-  }//pärast 8 ja enne 16
-  if($hournow >= 16 and $hournow <= 18){
-	  $partofday = "puhkamise aeg";
-  }//pärast 16 ja enne 18
-  if($hournow >= 18 and $hournow <= 20){
-  $partofday = "pesemise aeg";
-  }//pärast 18 ja enne 19
-  if($hournow >= 20 and $hournow <=6){
-	  $partofday = "magamise aeg";
-  }//pärast 22 ja enne 6
+  //vaatame, mda vormist serverile saadetakse
+  var_dump($_POST);
   
- //jälgime semestri kulgu
+  $weekdaynameset = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
+  $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
+  
+  //küsime nädalapäeva
+  $weekdaynow = date("N");
+  //echo $weekdaynow;
+  
+  if($hournow < 6){
+	  $partofday = "uneaeg";
+  }
+  if($hournow >= 6 and $hournow < 8){
+	  $partofday = "hommik";
+  }
+  if($hournow >= 8 and $hournow < 18){
+	  $partofday = "õppimine";
+  }
+  if($hournow >= 18 and $hournow < 22){
+	  $partofday = "õhtu";
+  }
+  if($hournow >= 22){
+	  $partofday = "magamine";
+  }
+  
+  //jälgime semestri kulgu
   $semesterstart = new DateTime("2020-8-31");
   $semesterend = new DateTime("2020-12-13");
   $semesterduration = $semesterstart->diff($semesterend);
@@ -30,8 +79,7 @@
   //saime aja erinevuse objektina, seda niisama näidata ei saa
   $fromsemesterstartdays = $fromsemesterstart->format("%r%a");
   $semesterpercentage = 0;
-  
-  
+    
   $semesterinfo = "Semester kulgeb vastavalt akadeemilisele kalendrile.";
   if($semesterstart > $today){
 	  $semesterinfo = "Semester pole veel peale hakanud!";
@@ -50,20 +98,56 @@
 	  $semesterinfo = "Semester on läbi saanud!";
   }
   
-?>
-<!DOCTYPE html>
-<html lang="et">
-<head>
-  <meta charset="utf-8">
-  <title><?php echo $username; ?> programmeerib veebi</title>
+  //loen kataloogist piltide nimekirja
+  //$allfiles = scandir("../vp_pics/");
+  $allfiles = array_slice(scandir("../vp_pics/"), 2);
+  //echo $allfiles;  //massiivi nii näidata ei saa!!!
+  //var_dump($allfiles);
+  //$allpicfiles = array_slice($allfiles, 2);
+  //var_dump($allpicfiles);
+  $allpicfiles = [];
+  $picfiletypes = ["image/jpeg", "image/png"];
+  //käin kogu massiivi läbi ja kontrollin iga üksikut elementi, kas on sobiv fail ehk pilt
+  foreach ($allfiles as $file){
+	  $fileinfo = getImagesize("../vp_pics/" .$file);
+	  if(in_array($fileinfo["mime"], $picfiletypes) == true){
+		  array_push($allpicfiles, $file);
+	  }
+  }
+  
+  //paneme kõik pildid järjest ekraanile
+  //uurime, mitu pilti on ehk mitu faili on nimekirjas - massiivis
+  $piccount = count($allpicfiles);
+  //echo $piccount;
+  //$i = $i + 1;
+  //$i += 1;
+  //$i ++;
+  $imghtml = "";
+  for($i = 0; $i < $piccount; $i ++){
+	  //<img src="../img/vp_banner.png" alt="alt tekst">
+	  $imghtml .= '<img src="../vp_pics/' .$allpicfiles[$i] .'" ';
+	  $imghtml .= 'alt="Tallinna Ülikool">';
+  }
 
-</head>
-<body>
-  <h1><?php echo $username; ?></h1>
-  <p>See veebileht on loodud õppetöö kaigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
-  <p>See konkreetne leht on loodud veebiprogrammeerimise kursusel aasta 2020 sügissemestril <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
-  <p>Lehe avamise hetk: <?php echo $fulltimenow; ?>.</p>
-  <p><?php echo "Praegu on " .$partofday ."."; ?></p>
+  require("header.php");
+?>
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1><?php echo $username; ?> programmeerib veebi</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+  <p>Lehe avamise aeg: <?php echo $weekdaynameset[$weekdaynow - 1] .", " .$fulltimenow; ?>. 
+  <?php echo "Parajasti on " .$partofday ."."; ?></p>
   <p><?php echo $semesterinfo; ?></p>
+  <hr>
+  <?php echo $imghtml; ?>
+  <hr>
+  <form method="POST">
+    <label>Sisesta oma tänane mõttetu mõte!</label>
+	<input type="text" name="nonsens" placeholder="mõttekoht">
+	<input type="submit" value="Saada ära!" name="submitnonsens">
+  </form>
+  <hr>
+  <?php echo $nonsenshtml; ?>
 </body>
 </html>
